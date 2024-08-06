8caddf06e5089810cb6f742ca0b50fdd9b01ca90
abhishekkumar4553 <abhishekgupta45591@gmail.com>
2024-08-07 02:48:25 +0530

    prepared our app for deployment

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -23,6 +24,8 @@ cloudinary.config({
   api_secret: config.apiSecret,
 });
 
+const __dirname = path.resolve();
+
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -31,5 +34,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 export { app };
